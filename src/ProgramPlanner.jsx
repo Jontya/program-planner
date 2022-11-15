8d2fd9445c75a2eced4c98b6c269a12a7f1eb3ed
jonty <Jontyatkinson2002@gmail.com>
2022-11-15 17:54:37 +1100

    Setup app template

---
+++ src/ProgramPlanner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class ProgramPlanner extends React.Component {
+    render(){
+        return(
+            <div className="project-contianer">
+                test
+            </div>
+        );
+    }
+};
