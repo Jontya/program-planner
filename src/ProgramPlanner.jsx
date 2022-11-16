22ec49f5ffbd73ec7004c11323995ad2cf25dd6f
jonty <Jontyatkinson2002@gmail.com>
2022-11-16 11:40:01 +1100

    Added new files and design template

--- src/ProgramPlanner.jsx
+++ src/ProgramPlanner.jsx
@@ -4,7 +4,42 @@ export default class ProgramPlanner extends React.Component {
     render(){
         return(
             <div className="project-contianer">
-                test
+                <table className="table">
+                    <tr className="2021">
+                        <td>C1</td>
+                        <td>C2</td>
+                        <td>C3</td>
+                        <td>C4</td>
+                    </tr>
+                    <tr className="2022">
+                        <td>C1</td>
+                        <td>C2</td>
+                        <td>C3</td>
+                        <td>C4</td>
+                        
+                        <td>C5</td>
+                        <td>C6</td>
+                        <td>C7</td>
+                        <td>C8</td>
+                    </tr>
+                    <tr className="2023">
+                        <td>C1</td>
+                        <td>C2</td>
+                        <td>C3</td>
+                        <td>C4</td>
+            
+                        <td>C5</td>
+                        <td>C6</td>
+                        <td>C7</td>
+                        <td>C8</td>
+                    </tr>
+                    <tr className="2024">
+                        <td>C1</td>
+                        <td>C2</td>
+                        <td>C3</td>
+                        <td>C4</td>               
+                    </tr>
+                </table>
             </div>
         );
     }
